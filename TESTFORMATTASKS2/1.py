4c2ccaff0fc714223cd4fb3fea30c90ce5292c54
SzymonJasik21 <szymon.jasik@op.pl>
2026-01-15 00:04:17 +0100

    siema

--- TESTFORMATTASKS2/1.py
+++ TESTFORMATTASKS2/1.py
@@ -2,5 +2,7 @@ def f(data):
     return list(map(lambda x: x**3, data))
 
 if __name__ == "__main__":
-    numbers = list(range(1, 21))
-    print(f(numbers))
+    #numbers = list(range(1, 21))
+    #print(f(numbers))
+
+    print(f(list(range(1, 21))))
